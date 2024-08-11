c0e86d9199702ced3ef1a62ce523df7a7a1e658d
Exequiel Pagliari <exepa@hotmail.com>
2024-08-10 22:00:06 -0300

    Configuradas texturas de bee, cloud y tree

--- SFMLTimber/src/main.cpp
+++ SFMLTimber/src/main.cpp
@@ -26,15 +26,78 @@ int main()
 		****************************************
 		*/
 
+		//Creada Textura de Fondo
 		Texture textureBackground;
-
+		//Carga de Textutra de archivo
 		textureBackground.loadFromFile("../graphics/background.png");
-
+		//Creacción de Sprite
 		Sprite spriteBackground;
-	
+		//Carga de Textura en el Sprite
 		spriteBackground.setTexture(textureBackground);
-
+		//Seteo de posición del Sprite
 		spriteBackground.setPosition(0, 0);
+
+		Texture textureTree;
+
+		textureTree.loadFromFile("../graphics/tree.png");
+
+		Sprite spriteTree;
+
+		spriteTree.setTexture(textureTree);
+
+		spriteTree.setPosition(810, 0);
+
+		Texture beeTexture;
+
+		beeTexture.loadFromFile("../graphics/bee.png");
+
+		Sprite spriteBee;
+
+		spriteBee.setTexture(beeTexture);
+
+		spriteBee.setPosition(0, 800);
+
+		bool beeActive = false;
+
+		float beeSpeed = 0.0f;
+
+		Texture cloudTexture;
+
+		cloudTexture.loadFromFile("../graphics/cloud.png");
+
+		Sprite spriteCloud1;
+
+		Sprite spriteCloud2;
+
+		Sprite spriteCloud3;
+
+		spriteCloud1.setTexture(cloudTexture);
+
+		spriteCloud2.setTexture(cloudTexture);
+
+		spriteCloud3.setTexture(cloudTexture);
+
+		spriteCloud1.setPosition(0, 0);
+
+		spriteCloud2.setPosition(0, 250);
+
+		spriteCloud3.setPosition(0, 500);
+
+
+		bool cloud1Active = false;
+
+		bool cloud2Active = false;
+
+		bool cloud3Active = false;
+
+
+		float cloud1Speed = 0.0f;
+
+		float cloud2Speed = 0.0f;
+
+		float cloud3Speed = 0.0f;
+
+
 
 		/*
 		****************************************
@@ -46,6 +109,8 @@ int main()
 		// Draw our game scene here
 		// 
 		window.draw(spriteBackground);
+
+		window.draw(spriteTree);
 		// 
 		// Show everything we just drew
 		window.display();
